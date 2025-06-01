df01966cb35bf9fd30199cf67a21e66b45d1de6c
Nica Sorin Nicusor <164754634+NicaSorinNicusor02@users.noreply.github.com>
2025-06-01 22:03:52 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -12,15 +12,11 @@ class SignUpView(CreateView):
     template_name = "registration/register.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #adauga centrele comerciale ca JSON(format de schimb de date care utilizeaza text) in contextul templateului
-        context['shopping_centers_json'] = json.dumps(SHOPPING_CENTERS)#converteste structura de date sub 
-        #forma de dictionar intr-un sir de caractere in format JSON, este utilizat mai tarziu in register.htmlS
+        context['shopping_centers_json'] = json.dumps(SHOPPING_CENTERS)
         return context
-
 
 def get_shopping_centers(request):
     city = request.GET.get('city', None)
     if city in SHOPPING_CENTERS:
-        # returneaza lista de centre comerciale pentru orasul selectat
         return JsonResponse(SHOPPING_CENTERS[city], safe=False)
     return JsonResponse([], safe=False)
