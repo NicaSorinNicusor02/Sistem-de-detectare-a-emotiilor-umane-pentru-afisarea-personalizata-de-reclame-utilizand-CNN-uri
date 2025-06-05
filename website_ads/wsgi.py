5b3632322827c566d3fdd857418e9fddc0ea27cb
Nica Sorin Nicusor <164754634+NicaSorinNicusor02@users.noreply.github.com>
2025-06-05 21:35:21 +0300

    Update wsgi.py

--- website_ads/wsgi.py
+++ website_ads/wsgi.py
@@ -1,8 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 from website_ads.admin import admin_site
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website_ads.settings')
-
 application = get_wsgi_application()
 admin_site.name = 'admin'
